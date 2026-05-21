2540dae229e497034418f2683a42373cd8ec5866
JuankHaro23 <juanchof_23@outlook.com>
2026-05-21 19:50:00 +0000

    chore: Inicialización completa de Firebase Functions y arreglo de index

---
+++ functions/src/index.ts
@@ -0,0 +1,9 @@
+import * as admin from "firebase-admin";
+
+// Inicializamos Firebase Admin para tener acceso a la base de datos
+admin.initializeApp();
+
+// Aquí exportaremos los módulos (comentados hasta que tengan código)
+// export * from "./auth/auth.triggers";
+// export * from "./catalog/catalog.functions";
+// export * from "./checkout/checkout.functions";
